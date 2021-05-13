f6fdd854286dcfd0a742a63e8b0fdfd064105740
Davidson <Joe.Davidson@ipfin.co.uk>
2021-05-13 08:43:22 +0100

    Initial

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/joe-davidson1802/go-site
+
+go 1.16
+
+require (
+	github.com/a-h/templ v0.0.0-20210511195301-6277e8780794 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	golang.org/x/tools/gopls v0.6.11 // indirect
+	mvdan.cc/gofumpt v0.1.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type Person struct {
+	Name string
+}
+
+func getPeople() []Person {
+	return []Person{
+		{Name: "Joe"},
+		{Name: "Bob"},
+	}
+}
+
+func getPerson(id int) (Person, error) {
+	people := getPeople()
+
+	if id < 0 || id >= len(people) {
+		return Person{}, fmt.Errorf("Could not find person at index: %d", id)
+	}
+
+	return people[id], nil
+
+}
+
+func TurboFrame(id string) func(w http.ResponseWriter, f func(http.ResponseWriter)) {
+	return func(w http.ResponseWriter, f func(http.ResponseWriter)) {
+		open := fmt.Sprintf("<turbo-frame id=\"%s\">", id)
+		io.WriteString(w, open)
+		f(w)
+		io.WriteString(w, "</turbo-frame>")
+	}
+}
+
+func HandleGetPeople(w http.ResponseWriter, r *http.Request) {
+	people := getPeople()
+
+	rend := func(w http.ResponseWriter) {
+		if err := renderPeople(r.Context(), w, people); err != nil {
+			log.Println("error", err)
+		}
+	}
+
+	//rend(w)
+	TurboFrame("people")(w, rend)
+}
+
+func HandleGetPerson(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+
+	id, err := strconv.Atoi(vars["id"])
+
+	if err != nil {
+		log.Println("error", err)
+		http.NotFound(w, r)
+		return
+	}
+
+	p, err := getPerson(id)
+
+	if err != nil {
+		log.Println("error", err)
+		http.NotFound(w, r)
+		return
+	}
+
+	if err := renderPerson(r.Context(), w, p); err != nil {
+		log.Println("error", err)
+	}
+}
+func htmlMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/html; charset=UTF-8")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.Use(htmlMiddleware)
+
+	r.HandleFunc("/people", HandleGetPeople)
+	r.HandleFunc("/person/{id}", HandleGetPerson)
+
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("public")))
+
+	http.Handle("/", r)
+	fmt.Println("Listening")
+	http.ListenAndServe(":8082", nil)
+}
